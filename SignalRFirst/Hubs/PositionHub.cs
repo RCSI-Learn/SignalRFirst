b0ebcd416d5a02cb9c394598d3f87b5b02b7b8a1
Roberto Carlos Siles Inca <rcsi.learn@gmail.com>
2022-10-17 21:22:59 -0400

    -First commit

---
+++ SignalRFirst/Hubs/PositionHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRFirst.Hubs {
+	public class PositionHub : Hub {
+		public async Task SendPosition(int left, int top) {
+			await Clients.Others.SendAsync("ReceivePosition", left, top);
+		}
+	}
+}
